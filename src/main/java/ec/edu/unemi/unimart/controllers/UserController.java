c42052eba5cb0dc2d6cff6de9db225c7a8a542d3
Víctor Arreaga <arreagav17@gmail.com>
2023-07-18 19:09:54 -0500

    Endpoints `save`, `findById`, `getAll`

---
+++ src/main/java/ec/edu/unemi/unimart/controllers/UserController.java
@@ -0,0 +1,48 @@
+package ec.edu.unemi.unimart.controllers;
+
+import ec.edu.unemi.unimart.dtos.UserDto;
+import ec.edu.unemi.unimart.services.user.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping
+    ResponseEntity<List<UserDto>> getAll() {
+        return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    ResponseEntity<HttpHeaders> save(@RequestBody UserDto userDto) {
+        UUID articleId = userService.save(userDto);
+        return new ResponseEntity<>(this.getHttpHeaders(articleId), HttpStatus.CREATED);
+    }
+    private HttpHeaders getHttpHeaders(UUID userId) {
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userId)
+                .toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return headers;
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<UserDto> findById(@PathVariable UUID id) {
+        return userService.findById(id)
+                .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
