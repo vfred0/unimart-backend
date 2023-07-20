8a6bc7934a11d085c6959db79bcbf3b7041ce20b
Víctor Arreaga <arreagav17@gmail.com>
2023-07-19 19:18:55 -0500

    Fix mapping with `@Getter` and `@Setter`

--- src/main/java/ec/edu/unemi/unimart/dtos/UserDto.java
+++ src/main/java/ec/edu/unemi/unimart/dtos/UserDto.java
@@ -1,14 +1,20 @@
 package ec.edu.unemi.unimart.dtos;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
 import java.util.UUID;
 
-public record UserDto(
-        UUID id,
-        String photo,
-        String name,
-        String about,
-        Integer numberOfExchanges,
-        Double rating,
-        String numberWhatsapp
-) {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter @Setter
+public class UserDto {
+    UUID id;
+    String photo;
+    String name;
+    String about;
+    Integer numberOfExchanges;
+    Double rating;
+    String numberWhatsapp;
 }
