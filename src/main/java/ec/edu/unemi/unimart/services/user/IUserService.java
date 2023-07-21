22f79153ea10a280cb9638ec30c5b82f848c1271
Víctor Arreaga <arreagav17@gmail.com>
2023-07-21 13:17:52 -0500

    Removed `findById`

--- src/main/java/ec/edu/unemi/unimart/services/user/IUserService.java
+++ src/main/java/ec/edu/unemi/unimart/services/user/IUserService.java
@@ -3,10 +3,7 @@ package ec.edu.unemi.unimart.services.user;
 import ec.edu.unemi.unimart.dtos.UserDto;
 import ec.edu.unemi.unimart.services.crud.ICrudService;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserService extends ICrudService<UserDto, UUID> {
-
-    Optional<UserDto> findById(UUID id);
 }
