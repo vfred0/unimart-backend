dfed8322d2ab4a2726dfcac278427fb0b8b00774
Víctor Arreaga <arreagav17@gmail.com>
2023-07-21 13:17:11 -0500

    Added `findById`

--- src/main/java/ec/edu/unemi/unimart/controllers/ArticleController.java
+++ src/main/java/ec/edu/unemi/unimart/controllers/ArticleController.java
@@ -25,11 +25,31 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.getAll());
     }
 
+    @GetMapping("/{id}")
+    ResponseEntity<ArticleDto> findById(@PathVariable UUID id) {
+        return articleService.findById(id)
+                .map(articleDto -> new ResponseEntity<>(articleDto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping
     ResponseEntity<HttpHeaders> save(@RequestBody ArticleDto articleDto) {
-        UUID articleId = articleService.save(articleDto).id();
+        UUID articleId = articleService.save(articleDto).getId();
         return new ResponseEntity<>(this.getHttpHeaders(articleId), HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    ResponseEntity<HttpHeaders> update(@PathVariable UUID id, @RequestBody ArticleDto articleDto) {
+        UUID articleId = articleService.update(id, articleDto).getId();
+        return new ResponseEntity<>(this.getHttpHeaders(articleId), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<HttpHeaders> delete(@PathVariable UUID id) {
+        articleService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 
     private HttpHeaders getHttpHeaders(UUID userId) {
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
