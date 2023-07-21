52ca4d4a6fd7e460ee42458e1273c310aad9a5ab
Víctor Arreaga <arreagav17@gmail.com>
2023-07-21 13:18:08 -0500

    Format file

--- src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
@@ -1,5 +1,8 @@
 package ec.edu.unemi.unimart.repositories;
- import ec.edu.unemi.unimart.models.Article;
- import java.util.UUID;
+
+import ec.edu.unemi.unimart.models.Article;
+
+import java.util.UUID;
+
 public interface IArticleRepository extends IRepository<Article, UUID> {
 }
