3666a013468ebb8bcd55efd41b983567c1f7d948
Víctor Arreaga <arreagav17@gmail.com>
2023-07-21 19:43:22 -0500

    Added to `CORS` and `Mapper`
    Took 18 seconds

---
+++ src/main/java/ec/edu/unemi/unimart/config/Config.java
@@ -0,0 +1,24 @@
+package ec.edu.unemi.unimart.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
