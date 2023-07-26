826830fdf036a5cacaca8a0b02455ff459113b94
Víctor Arreaga <arreagav17@gmail.com>
2023-07-25 19:16:44 -0500

    Added `proposedArticles`
    Took 9 seconds

--- src/main/java/ec/edu/unemi/unimart/services/article/IArticleService.java
+++ src/main/java/ec/edu/unemi/unimart/services/article/IArticleService.java
@@ -1,7 +1,7 @@
 package ec.edu.unemi.unimart.services.article;
 
 import ec.edu.unemi.unimart.dtos.article.ArticleDto;
-import ec.edu.unemi.unimart.dtos.article.ArticleSaveOrCardDto;
+import ec.edu.unemi.unimart.dtos.article.ArticleCardDto;
 import ec.edu.unemi.unimart.dtos.article.SuggestArticleDto;
 import ec.edu.unemi.unimart.services.crud.ICrudService;
 import ec.edu.unemi.unimart.models.enums.Category;
@@ -13,7 +13,9 @@ import java.util.UUID;
 public interface IArticleService extends ICrudService<ArticleDto, UUID> {
     List<ArticleDto> search(String title, Category category, State state);
 
-    List<ArticleSaveOrCardDto> findByUserId(UUID id);
+    List<ArticleCardDto> findByUserId(UUID id);
 
     UUID addProposal(SuggestArticleDto suggestArticleDto);
+
+    List<ArticleCardDto> proposedArticles(UUID id);
 }
