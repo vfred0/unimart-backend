1474bb36161a5973891e3d03b56b045e3a716ebf
Víctor Arreaga <arreagav17@gmail.com>
2023-07-26 16:45:01 -0500

    Refactor
    Took 18 seconds

--- src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
@@ -1,7 +1,13 @@
 package ec.edu.unemi.unimart.repositories;
 
+import ec.edu.unemi.unimart.dtos.article.ArticleCardDto;
 import ec.edu.unemi.unimart.models.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IArticleRepository extends IRepository<Article, UUID> {
