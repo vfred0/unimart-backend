d48c55706f7f6a19c899b61452aeedcf6e4f6ec0
Víctor Arreaga <arreagav17@gmail.com>
2023-07-27 14:49:27 -0500

    Refactor
    Took 15 seconds

--- src/main/resources/database/functions.sql
+++ src/main/resources/database/functions.sql
@@ -37,7 +37,7 @@ $$
 DECLARE
     _article_id     UUID;
     count_proposals SMALLINT;
-
+    is_proposed     BOOLEAN;
 BEGIN
     _article_id := CAST((SELECT pa.proposed_article_id
                          FROM articles a,
@@ -45,15 +45,59 @@ BEGIN
                          WHERE a.id = p_article_id
                            AND a.id = pa.article_id) AS UUID);
 
-    count_proposals := (SELECT COUNT(*)
-                        FROM proposed_articles
-                        WHERE proposed_article_id = _article_id);
+
+    IF _article_id IS NULL THEN
+        is_proposed := TRUE;
+        _article_id := CAST((SELECT pa.article_id
+                             FROM articles a,
+                                  proposed_articles pa
+                             WHERE a.id = p_article_id
+                               AND a.id = pa.proposed_article_id) AS UUID);
+
+        count_proposals := (SELECT COUNT(*)
+                            FROM proposed_articles
+                            WHERE article_id = _article_id);
+    ELSE
+        count_proposals := (SELECT COUNT(*)
+                            FROM proposed_articles
+                            WHERE proposed_article_id = _article_id);
+    END IF;
+    RAISE NOTICE 'article_id: %', _article_id;
+    RAISE NOTICE 'count_proposals: %', count_proposals;
     count_proposals := count_proposals - 1;
+    RAISE NOTICE 'count_proposals: %', count_proposals;
 
     IF count_proposals = 0 THEN
+        IF is_proposed THEN
+            _article_id := p_article_id;
+        END IF;
         UPDATE articles
         SET type_article = 'PUBLISHED'
         WHERE id = _article_id;
     END IF;
 END;
 $$;
+
+SELECT fn_update_type_article_from_deleted('839907cf-72d3-4754-8c9d-57a8ff79d22b')
+SELECT fn_update_type_article_from_deleted('0cb991f7-3e47-470b-8328-fd132304f818')
+
+
+
+CREATE OR REPLACE FUNCTION fn_exists_proposed_article(p_article_id UUID) RETURNS BOOLEAN
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+
+    RETURN EXISTS((SELECT 1
+                   FROM proposed_articles pa
+                   WHERE pa.proposed_article_id = p_article_id));
+END;
+$$;
+
+
+SELECT a.id
+FROM articles a,
+     proposed_articles pa
+WHERE pa.article_id = a.id
+  AND pa.proposed_article_id = '839907cf-72d3-4754-8c9d-57a8ff79d22b'
