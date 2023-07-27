cda40f80ac0c3972dc80a1f01621a199f1cfc63e
Víctor Arreaga <arreagav17@gmail.com>
2023-07-26 21:40:17 -0500

    Fix: get articles by user id
    Took 7 minutes

--- src/main/resources/database/functions.sql
+++ src/main/resources/database/functions.sql
@@ -10,9 +10,8 @@ BEGIN
                        articles a,
                        proposed_articles pa
                   WHERE u.id = p_user_id
+                    AND u.id = a.user_id
                     AND a.id = pa.proposed_article_id
                     AND pa.article_id = p_proposed_article);
 END;
 $$ LANGUAGE plpgsql;
-
-DROP FUNCTION IF EXISTS fn_user_has_made_proposed_changes(p_user_id UUID, p_proposed_article UUID)
