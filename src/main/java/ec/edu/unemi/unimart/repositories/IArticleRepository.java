273e38389663f10164f5f94bafeda5a71c866465
Víctor Arreaga <arreagav17@gmail.com>
2023-07-27 14:49:12 -0500

    Added `deleteProposedArticleById`
    Took 29 seconds

--- src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IArticleRepository.java
@@ -1,29 +1,27 @@
 package ec.edu.unemi.unimart.repositories;
 
-import ec.edu.unemi.unimart.dtos.article.ArticleCardDto;
 import ec.edu.unemi.unimart.models.Article;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface IArticleRepository extends IRepository<Article, UUID> {
-    @Query(value = "SELECT a.* FROM articles a " +
-            "JOIN proposed_articles ap ON a.id = ap.article_id " +
-            "WHERE ap.article_id = :id", nativeQuery = true)
-    List<ArticleCardDto> findProposedArticlesByArticleId(UUID id);
-
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM proposed_articles WHERE article_id = :articleId AND proposed_article_id = :proposedArticleId", nativeQuery = true)
     void deleteProposedArticleByArticleId(UUID articleId, UUID proposedArticleId);
-    @Query(value = "SELECT fn_user_has_made_proposed(cast(:userId as uuid), cast(:articleId as uuid))", nativeQuery = true)
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM proposed_articles WHERE proposed_article_id = :proposedArticleId", nativeQuery = true)
+    void deleteProposedArticleById(UUID proposedArticleId);
+
+    @Query(value = "SELECT fn_user_has_made_proposed(CAST(:userId AS UUID), CAST(:articleId AS UUID))", nativeQuery = true)
     Boolean userHasMadeProposed(UUID userId, UUID articleId);
 
-    @Query(value = "SELECT fn_update_type_article_from_deleted(cast(:articleId as uuid))", nativeQuery = true)
+    @Query(value = "SELECT fn_update_type_article_from_deleted(CAST(:articleId AS UUID))", nativeQuery = true)
     void updateTypeArticleFromDeleted(UUID articleId);
 
     @Query(value = "SELECT fn_exists_proposed_article(CAST(:proposedArticleId AS UUID))", nativeQuery = true)
