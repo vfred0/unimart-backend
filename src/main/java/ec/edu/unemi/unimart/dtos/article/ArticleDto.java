6a6d411a88178bf3f95c5c0a551a0b4d8a4b1aa8
Víctor Arreaga <arreagav17@gmail.com>
2023-07-29 13:13:57 -0500

    Refactor, default values
    Took 28 seconds

--- src/main/java/ec/edu/unemi/unimart/dtos/article/ArticleDto.java
+++ src/main/java/ec/edu/unemi/unimart/dtos/article/ArticleDto.java
@@ -3,6 +3,9 @@ package ec.edu.unemi.unimart.dtos.article;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ec.edu.unemi.unimart.dtos.UserDto;
+import ec.edu.unemi.unimart.models.enums.Category;
+import ec.edu.unemi.unimart.models.enums.State;
+import ec.edu.unemi.unimart.models.enums.TypeArticle;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -20,24 +23,40 @@ import java.util.UUID;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ArticleDto {
     UUID id;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     UUID userId;
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     UserDto user;
+
     @Size(max = 60, message = "El título no puede tener más de 60 caracteres")
     String title;
+
     @Size(max = 250, message = "La descripción no puede tener más de 100 caracteres")
     String description;
+
     List<String> images;
+
+    @Builder.Default
     @Size(max = 50, message = "La categoría no puede tener más de 20 caracteres")
-    String category;
+    String category = Category.TEXT_BOOKS_EDUCATIONAL_MATERIAL.getName();
+
+    @Builder.Default
     @Size(max = 15, message = "El estado no puede tener más de 15 caracteres")
-    String state;
-    @Size(max = 10, message = "El género no puede tener más de 10 caracteres")
-    @Null
-    String gender;
+    String state = State.NEW.getName();
+
+    @Builder.Default
     @Size(max = 10, message = "El tipo de artículo no puede tener más de 10 caracteres")
-    String typeArticle;
+    String typeArticle = TypeArticle.PUBLISHED.getName();
+
+    @Null
+    @Size(max = 10, message = "El género no puede tener más de 10 caracteres")
+    String gender;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     Short numbersProposals;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     LocalDateTime date;
 }
