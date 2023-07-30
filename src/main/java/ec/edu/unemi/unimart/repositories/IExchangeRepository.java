3d82d59be077f095e62599a5b1e64e755c4551f6
Víctor Arreaga <arreagav17@gmail.com>
2023-07-29 19:10:41 -0500

    Search in repository
    Took 28 seconds

--- src/main/java/ec/edu/unemi/unimart/repositories/IExchangeRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IExchangeRepository.java
@@ -2,19 +2,17 @@ package ec.edu.unemi.unimart.repositories;
 
 import ec.edu.unemi.unimart.dtos.ExchangeDto;
 import ec.edu.unemi.unimart.models.Exchange;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface IExchangeRepository extends IRepository<Exchange, UUID> {
-    @Query("SELECT e FROM Exchange e, ProposedArticle pa WHERE e.id = pa.exchange.id AND pa.article.id = :userId AND e.isAccepted = false")
-    List<ExchangeDto> findByUserId(UUID userId);
 
-    @Modifying
-    @Transactional
-    @Query("UPDATE ProposedArticle SET exchange.id = :exchangeId WHERE article.id = :articleId AND proposedArticle.id = :proposedArticle")
-    void setExchangeId(UUID articleId, UUID proposedArticle, UUID exchangeId);
+    @Query("SELECT e FROM Article a, ProposedArticle pa, Exchange e WHERE a.user.id = :userId AND (a.id = pa.article.id OR a.id = pa.proposedArticle.id) AND pa.exchange.id IS NOT NULL")
+    List<Exchange> findByUserId(UUID userId);
+
+    @Query(value = "SELECT fn_update_articles_from_accept_exchange(CAST(:exchangeId AS UUID), CAST(:articleId AS UUID), CAST(:proposedArticleId AS UUID))", nativeQuery = true)
+    void updateArticlesFromAcceptExchange(UUID exchangeId, UUID articleId, UUID proposedArticleId);
+
 }
