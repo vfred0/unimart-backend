4c5daf80c74d600d4ba689f3123786c464b718b3
Víctor Arreaga <arreagav17@gmail.com>
2023-07-29 19:09:42 -0500

    New query's
    Took 3 seconds

--- src/main/resources/database/querys.sql
+++ src/main/resources/database/querys.sql
@@ -1,4 +1,10 @@
 DELETE FROM proposed_articles;
+-- DELETE FROM exchanges;
 DELETE FROM articles_images;
 DELETE FROM articles;
 DELETE FROM users;
+
+
+select * from articles;
+select * from proposed_articles;
+select * from exchanges;
