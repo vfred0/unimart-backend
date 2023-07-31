b561d83213bd92f76ba241656462283385923edf
Víctor Arreaga <arreagav17@gmail.com>
2023-07-31 18:45:51 -0500

    Refactor and added setExchangeMade
    Took 24 seconds

--- src/main/java/ec/edu/unemi/unimart/services/exchange/IExchangeService.java
+++ src/main/java/ec/edu/unemi/unimart/services/exchange/IExchangeService.java
@@ -1,14 +1,17 @@
 package ec.edu.unemi.unimart.services.exchange;
 
-import ec.edu.unemi.unimart.dtos.ExchangeDto;
-import ec.edu.unemi.unimart.dtos.ExchangeSaveDto;
+import ec.edu.unemi.unimart.dtos.exchange.ExchangeDto;
+import ec.edu.unemi.unimart.dtos.article.ProposedArticleDto;
+import ec.edu.unemi.unimart.dtos.rating.RatingDto;
 import ec.edu.unemi.unimart.services.crud.ICrudService;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface IExchangeService extends ICrudService<ExchangeDto, UUID> {
-    UUID save(ExchangeSaveDto exchangeSaveDto);
+    UUID save(ProposedArticleDto proposedArticleDto);
 
     List<ExchangeDto> findByUserId(UUID id);
+
+    UUID setExchangeMade(UUID exchangeId, RatingDto ratingDto);
 }
