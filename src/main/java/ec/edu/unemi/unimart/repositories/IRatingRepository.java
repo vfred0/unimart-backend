8be7320afcbf2f36b6cc156c8d8456f04bfecce2
Víctor Arreaga <arreagav17@gmail.com>
2023-07-31 18:49:30 -0500

    Rating service and repository
    Took 42 seconds

--- src/main/java/ec/edu/unemi/unimart/repositories/IRatingRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IRatingRepository.java
@@ -1,8 +1,16 @@
 package ec.edu.unemi.unimart.repositories;
 
+import ec.edu.unemi.unimart.dtos.rating.RatingCardDto;
 import ec.edu.unemi.unimart.models.Rating;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IRatingRepository extends IRepository<Rating, UUID>{
+//    @Query(value="SELECT * from fn_get_rating_by_user_id(:userId)", nativeQuery = true)
+//    List<RatingCardDto> getByUserId(UUID userId);
+
+//    @Query("SELECT u.name  from User u, Rating r where u.id = r.userId and u.id = :userId")
+    List<RatingCardDto> findByUserId(UUID userId);
 }
--- src/main/java/ec/edu/unemi/unimart/services/rating/IRatingService.java
+++ src/main/java/ec/edu/unemi/unimart/services/rating/IRatingService.java
@@ -1,9 +1,12 @@
 package ec.edu.unemi.unimart.services.rating;
 
-import ec.edu.unemi.unimart.dtos.RatingDto;
+import ec.edu.unemi.unimart.dtos.rating.RatingCardDto;
+import ec.edu.unemi.unimart.dtos.rating.RatingDto;
 import ec.edu.unemi.unimart.services.crud.ICrudService;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IRatingService extends ICrudService<RatingDto, UUID> {
+    List<RatingCardDto> getByUserId(UUID userId);
 }
