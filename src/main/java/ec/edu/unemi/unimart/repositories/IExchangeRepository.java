0c4554f47a175731e694fcf5229b92e4dcf576fc
Víctor Arreaga <arreagav17@gmail.com>
2023-07-31 18:45:27 -0500

    New methods for details of exchanges
    Took 1 minute

--- src/main/java/ec/edu/unemi/unimart/repositories/IExchangeRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IExchangeRepository.java
@@ -1,6 +1,5 @@
 package ec.edu.unemi.unimart.repositories;
 
-import ec.edu.unemi.unimart.dtos.ExchangeDto;
 import ec.edu.unemi.unimart.models.Exchange;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,10 +8,12 @@ import java.util.UUID;
 
 public interface IExchangeRepository extends IRepository<Exchange, UUID> {
 
-    @Query("SELECT e FROM Article a, ProposedArticle pa, Exchange e WHERE a.user.id = :userId AND (a.id = pa.article.id OR a.id = pa.proposedArticle.id) AND pa.exchange.id IS NOT NULL")
-    List<Exchange> findByUserId(UUID userId);
+    @Query(value = "SELECT * FROM fn_get_exchange_by_user_id(CAST(:userId AS UUID))", nativeQuery = true)
+    List<Object[]> findByUserId(UUID userId);
 
-    @Query(value = "SELECT fn_update_articles_from_accept_exchange(CAST(:exchangeId AS UUID), CAST(:articleId AS UUID), CAST(:proposedArticleId AS UUID))", nativeQuery = true)
-    void updateArticlesFromAcceptExchange(UUID exchangeId, UUID articleId, UUID proposedArticleId);
+    @Query(value = "SELECT fn_update_articles_from_accept_exchange(CAST(:exchangeId AS UUID), CAST(:proposedArticleId AS UUID))", nativeQuery = true)
+    void updateArticlesFromAcceptExchange(UUID exchangeId, UUID proposedArticleId);
 
+    @Query(value = "SELECT fn_update_articles_from_made_exchange(CAST(:exchangeId AS UUID), CAST(:ratingId AS UUID), CAST(:userId AS UUID))", nativeQuery = true)
+    void updateArticlesFromMadeExchange(UUID exchangeId, UUID ratingId, UUID userId);
 }
