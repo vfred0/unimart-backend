6e817a46d6b0ec682686035391d875676d078e1e
Víctor Arreaga <arreagav17@gmail.com>
2023-07-31 18:50:07 -0500

    Refactor
    Took 10 seconds

--- src/main/java/ec/edu/unemi/unimart/services/exchange/ExchangeService.java
+++ src/main/java/ec/edu/unemi/unimart/services/exchange/ExchangeService.java
@@ -1,30 +1,30 @@
 package ec.edu.unemi.unimart.services.exchange;
 
-import ec.edu.unemi.unimart.dtos.ExchangeDto;
+import ec.edu.unemi.unimart.dtos.exchange.ExchangeDto;
 import ec.edu.unemi.unimart.dtos.article.ProposedArticleDto;
+import ec.edu.unemi.unimart.dtos.rating.RatingDto;
 import ec.edu.unemi.unimart.mappers.Mapper;
-import ec.edu.unemi.unimart.models.Article;
 import ec.edu.unemi.unimart.models.Exchange;
-import ec.edu.unemi.unimart.repositories.IArticleRepository;
-import ec.edu.unemi.unimart.repositories.IExchangeRepository;
-import ec.edu.unemi.unimart.repositories.IProposedArticleRepository;
-import ec.edu.unemi.unimart.repositories.IUserRepository;
+import ec.edu.unemi.unimart.models.Rating;
+import ec.edu.unemi.unimart.repositories.*;
 import ec.edu.unemi.unimart.services.crud.CrudService;
+import ec.edu.unemi.unimart.services.rating.IRatingService;
+import jakarta.transaction.Transactional;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 public class ExchangeService extends CrudService<Exchange, ExchangeDto, UUID> implements IExchangeService {
-    private final IArticleRepository articleRepository;
-    private final IUserRepository userRepository;
+    private final IRatingService ratingService;
     private final IProposedArticleRepository proposedArticleRepository;
 
-    public ExchangeService(Mapper mapper, IExchangeRepository repository, IArticleRepository articleRepository, IUserRepository userRepository, IProposedArticleRepository proposedArticleRepository) {
+    public ExchangeService(Mapper mapper, IExchangeRepository repository, IRatingService ratingService, IProposedArticleRepository proposedArticleRepository) {
         super(mapper, repository, Exchange.class, ExchangeDto.class);
-        this.articleRepository = articleRepository;
-        this.userRepository = userRepository;
+        this.ratingService = ratingService;
         this.proposedArticleRepository = proposedArticleRepository;
     }
 
@@ -32,21 +32,40 @@ public class ExchangeService extends CrudService<Exchange, ExchangeDto, UUID> im
         return (IExchangeRepository) super.getRepository();
     }
 
+    @Transactional
     @Override
     public UUID save(ProposedArticleDto proposedArticleDto) {
-        Article article = articleRepository.findById(proposedArticleDto.getArticleId()).orElseThrow(() -> new RuntimeException("Article not found"));
-        Article proposedArticle = articleRepository.findById(proposedArticleDto.getProposedArticleId()).orElseThrow(() -> new RuntimeException("Article proposed not found"));
-        Exchange exchange = new Exchange();
-        exchange.setValues(article, proposedArticle);
-        UUID exchangeId = this.getRepository().save(exchange).getId();
-        this.getRepository().updateArticlesFromAcceptExchange(exchangeId, article.getId(), proposedArticle.getId());
+        UUID exchangeId = this.getRepository().save(new Exchange()).getId();
+        this.getRepository().updateArticlesFromAcceptExchange(exchangeId, proposedArticleDto.getProposedArticleId());
         return exchangeId;
     }
 
     @Override
     public List<ExchangeDto> findByUserId(UUID userId) {
-        this.userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found: " + userId));
-        return this.getMapper().toDtos(this.getRepository().findByUserId(userId), ExchangeDto.class);
+        List<ExchangeDto> exchangeDtos = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        this.getRepository().findByUserId(userId).forEach(exchange -> {
+            ExchangeDto exchangeDto = new ExchangeDto();
+            exchangeDto.setId(UUID.fromString(exchange[0].toString()));
+            exchangeDto.setUserId(UUID.fromString(exchange[1].toString()));
+            exchangeDto.setUserName(exchange[2].toString());
+            exchangeDto.setUserPhoto(exchange[3].toString());
+            exchangeDto.setArticleToExchange(exchange[4].toString());
+            exchangeDto.setArticleToReceive(exchange[5].toString());
+            exchangeDto.setHasBeenRated(Boolean.parseBoolean(exchange[6].toString()));
+            exchangeDto.setIsDiscarded(Boolean.parseBoolean(exchange[7].toString()));
+//            exchangeDto.setDate(new PrettyTime(new Locale("es")).format(LocalDateTime.parse(exchange[8].toString(), formatter)));
+            exchangeDto.setDate(exchange[8].toString());
+            exchangeDtos.add(exchangeDto);
+        });
+        return exchangeDtos;
+    }
+
+    @Override
+    public UUID setExchangeMade(UUID exchangeId, RatingDto ratingDto) {
+        Exchange exchange = this.getRepository().findById(exchangeId).orElseThrow(() -> new RuntimeException("Exchange not found: " + exchangeId));
+        this.getRepository().updateArticlesFromMadeExchange(exchangeId, this.ratingService.save(ratingDto).getId(), ratingDto.getUserId());
+        return this.getRepository().save(exchange).getId();
     }
 
     @Override
