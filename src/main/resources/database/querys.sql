c1ebb8c0ebf953899685bd9935c8b2583fec3a84
Víctor Arreaga <arreagav17@gmail.com>
2023-07-31 18:48:48 -0500

    New query's
    Took 25 seconds

--- src/main/resources/database/querys.sql
+++ src/main/resources/database/querys.sql
@@ -1,10 +1,17 @@
+DELETE FROM exchanges;
 DELETE FROM proposed_articles;
--- DELETE FROM exchanges;
 DELETE FROM articles_images;
 DELETE FROM articles;
 DELETE FROM users;
+DELETE FROM ratings;
 
 
-select * from articles;
-select * from proposed_articles;
-select * from exchanges;
+select u.id, u.name, u.username, a.id, a.title, a.category, a.type_article, a.numbers_proposals  from users u, articles a where u.id = a.user_id;
+SELECT *
+FROM proposed_articles;
+
+SELECT *
+FROM exchanges;
+
+SELECT *
+FROM ratings;
