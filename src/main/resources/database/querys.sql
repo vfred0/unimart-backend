66a92215826683bbd7cd27b265ddffb2e4538493
Víctor Arreaga <arreagav17@gmail.com>
2023-08-01 00:29:28 -0500

    Updated query's
    Took 17 seconds

--- src/main/resources/database/querys.sql
+++ src/main/resources/database/querys.sql
@@ -2,11 +2,11 @@ DELETE FROM exchanges;
 DELETE FROM proposed_articles;
 DELETE FROM articles_images;
 DELETE FROM articles;
-DELETE FROM users;
 DELETE FROM ratings;
+DELETE FROM users;
 
 
-select u.id, u.name, u.username, a.id, a.title, a.category, a.type_article, a.numbers_proposals  from users u, articles a where u.id = a.user_id;
+select u.id, u.number_of_exchanges, u.rating,  u.name, u.username, a.id, a.title, a.category, a.type_article, a.numbers_proposals  from users u, articles a where u.id = a.user_id;
 SELECT *
 FROM proposed_articles;
 
