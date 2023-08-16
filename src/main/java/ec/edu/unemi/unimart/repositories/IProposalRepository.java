8658f9858108aac7d4130a16e3fa20b8c39a8cf9
Víctor Arreaga <arreagav17@gmail.com>
2023-08-15 23:24:42 -0500

    Fixed: Added `@Transactional`
    Took 20 seconds

--- src/main/java/ec/edu/unemi/unimart/repositories/IProposalRepository.java
+++ src/main/java/ec/edu/unemi/unimart/repositories/IProposalRepository.java
@@ -8,12 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface IProposalRepository extends IRepository<Proposal, UUID> {
+    @Transactional
     @Modifying
     @Query("DELETE FROM Proposal pa WHERE pa.proposerArticle.id = :proposerArticleId")
     void deleteByProposerArticleId(UUID proposerArticleId);
 
     Proposal findByReceiverArticleIdAndProposerArticleId(UUID receiverArticleId, UUID proposerArticleId);
 
+    @Transactional
     @Modifying
     @Query(value = "CALL proc_delete_others_proposed_articles(:proposedArticleId, :receiverArticleId, :proposerArticleId)", nativeQuery = true)
     void deleteOthersProposals(UUID proposedArticleId, UUID receiverArticleId, UUID proposerArticleId);
