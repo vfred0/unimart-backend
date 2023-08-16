6cda48f21dd60637ee062dc2084b357bc6e7816f
Víctor Arreaga <arreagav17@gmail.com>
2023-08-15 23:16:52 -0500

    Added inserts
    Took 6 hours 44 minutes

---
+++ src/main/resources/database/articles.sql
@@ -0,0 +1,203 @@
+CREATE OR REPLACE FUNCTION get_random_user_id() RETURNS UUID AS
+$$
+DECLARE
+    user_id UUID;
+
+BEGIN
+    SELECT id INTO user_id FROM users ORDER BY RANDOM() LIMIT 1;
+    RETURN user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Libro de Matemáticas', 'Libro usado de matemáticas', 'TEXT_BOOKS_EDUCATIONAL_MATERIAL',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Libro de Historia', 'Libro nuevo de historia universal',
+        'TEXT_BOOKS_EDUCATIONAL_MATERIAL', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Libro de Matemáticas Avanzadas', 'Libro de matemáticas avanzadas usado',
+        'TEXT_BOOKS_EDUCATIONAL_MATERIAL',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Libro de Álgebra', 'Libro de álgebra nuevo', 'TEXT_BOOKS_EDUCATIONAL_MATERIAL', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Libro de Trigonometría', 'Libro de trigonometría usado',
+        'TEXT_BOOKS_EDUCATIONAL_MATERIAL', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Resmas de hojas', 'Paquete de 10 resmas de hojas', 'OFFICE_SUPPLIES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Lapiceras azules', 'Paquete de 20 lapiceras azules', 'OFFICE_SUPPLIES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Sacapuntas eléctrico', 'Sacapuntas eléctrico usado', 'OFFICE_SUPPLIES', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Cuadernos universitarios', 'Paquete de cuadernos universitarios rayados',
+        'OFFICE_SUPPLIES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Marcadores Sharpie', 'Paquete de marcadores Sharpie surtidos', 'OFFICE_SUPPLIES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Lapiceras rojas', 'Paquete de lapiceras rojas', 'OFFICE_SUPPLIES', 'NEW');
+
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'iPad Pro 11"', 'iPad Pro 11" usado', 'ELECTRONICS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Laptop Lenovo', 'Laptop Lenovo nueva', 'ELECTRONICS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'iMac 27"', 'iMac 27" usado', 'ELECTRONICS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'iMac 21"', 'iMac 21" nuevo', 'ELECTRONICS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'MacBook Pro 16"', 'MacBook Pro 16" usado', 'ELECTRONICS', 'USED');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Silla de escritorio', 'Silla de escritorio para oficina', 'FURNITURE_AND_DECORATION',
+        'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Cuadro decorativo', 'Cuadro decorativo para sala', 'FURNITURE_AND_DECORATION', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Mesa de centro', 'Mesa de centro de vidrio para sala', 'FURNITURE_AND_DECORATION',
+        'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Silla ejecutiva', 'Silla ejecutiva de oficina', 'FURNITURE_AND_DECORATION', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Silla de visitas', 'Silla de visitas para sala', 'FURNITURE_AND_DECORATION', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Silla para barra desayunador', 'Silla para barra de desayunador',
+        'FURNITURE_AND_DECORATION', 'NEW');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Vestido de fiesta', 'Vestido de fiesta para mujer', 'CLOTHING', 'NEW', 'FEMALE');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Traje para hombre', 'Traje azul para hombre', 'CLOTHING', 'USED', 'MALE');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Zapatillas Adidas', 'Zapatillas deportivas Adidas para hombre', 'CLOTHING', 'NEW',
+        'MALE');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Traje sastre mujer', 'Traje sastre para mujer', 'CLOTHING', 'NEW', 'FEMALE');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Traje chaqueta hombre', 'Traje chaqueta para hombre', 'CLOTHING', 'USED', 'MALE');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Traje baño mujer', 'Traje de baño para mujer', 'CLOTHING', 'NEW', 'FEMALE');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Balanza analítica', 'Balanza analítica de precisión', 'LABORATORY_MATERIAL', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Tubos de ensayo', 'Paquete de 50 tubos de ensayo nuevos', 'LABORATORY_MATERIAL', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Microscopio', 'Microscopio de laboratorio usado', 'LABORATORY_MATERIAL', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Centrífuga para laboratorio', 'Centrífuga para laboratorio', 'LABORATORY_MATERIAL',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Matraz erlenmeyer', 'Matraz erlenmeyer de vidrio nuevo', 'LABORATORY_MATERIAL', 'NEW');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Guitarra eléctrica', 'Guitarra eléctrica usada en buen estado', 'MUSICAL_INSTRUMENTS',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Saxofón', 'Saxofón para estudiante nuevo', 'MUSICAL_INSTRUMENTS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Flauta traversa', 'Flauta traversa usada para estudiante', 'MUSICAL_INSTRUMENTS',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Piano eléctrico', 'Piano eléctrico usado', 'MUSICAL_INSTRUMENTS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Batería acústica', 'Batería acústica 5 piezas', 'MUSICAL_INSTRUMENTS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Ukulele', 'Ukulele nuevo para principiantes', 'MUSICAL_INSTRUMENTS', 'NEW');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Pelota de fútbol', 'Pelota de fútbol oficial nueva', 'SPORTING_GOODS', 'NEW', 'UNISEX');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Pesas de gimnasio', 'Juego de pesas de gimnasio usadas', 'SPORTING_GOODS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Bicicleta MTB', 'Bicicleta MTB suspension completa', 'SPORTING_GOODS', 'NEW', 'UNISEX');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Patineta', 'Patineta profesional usada', 'SPORTING_GOODS', 'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Set de pesas', 'Set de pesas y barras para gimnasio', 'SPORTING_GOODS', 'USED');
+
+
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Set de óleo pintura', 'Set de pinturas al óleo nuevo', 'ARTS_HANDICRAFTS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Ganchillo', 'Kit de ganchillo con lana y agujas', 'ARTS_HANDICRAFTS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Set de acuarelas', 'Set de acuarelas con pinceles', 'ARTS_HANDICRAFTS', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Bastidor para óleo', 'Bastidor de madera para pintura al óleo', 'ARTS_HANDICRAFTS',
+        'USED');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Ganchillo tunecino', 'Kit de ganchillo tunecino con lana', 'ARTS_HANDICRAFTS', 'NEW');
+
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Muñeca Barbie', 'Muñeca Barbie nueva', 'TOYS_GAMES', 'NEW', 'FEMALE');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Rompecabezas 1000 piezas', 'Rompecabezas nuevo de 1000 piezas', 'TOYS_GAMES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Juego de química', 'Juego de química para niños', 'TOYS_GAMES', 'USED', 'UNISEX');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Rompecabezas 2000 piezas', 'Rompecabezas nuevo de 2000 piezas', 'TOYS_GAMES', 'NEW');
+
+INSERT INTO articles (user_id, title, description, category, state, gender)
+VALUES (get_random_user_id(), 'Rompecabezas 3000 piezas', 'Rompecabezas de 3000 piezas', 'TOYS_GAMES', 'NEW', 'UNISEX');
+
+INSERT INTO articles (user_id, title, description, category, state)
+VALUES (get_random_user_id(), 'Rompecabezas 5000 piezas', 'Rompecabezas nuevo de 5000 piezas', 'TOYS_GAMES', 'NEW');
---
+++ src/main/resources/database/articles_images.sql
@@ -0,0 +1,70 @@
+INSERT INTO article_images (article_id, image)
+VALUES ('9bad23aa-f869-4f25-ae57-d0a276410090', 'https://is.gd/u8Vva0'),
+       ('9bad23aa-f869-4f25-ae57-d0a276410090', 'https://is.gd/u8Vva0'),
+       ('b1b3a450-fdf4-4c40-b626-f63f8c27636e', 'https://is.gd/u8Vva0'),
+       ('dc889346-8c14-4174-b945-ed03b2542544', 'https://is.gd/u8Vva0'),
+       ('e5d68a85-211d-435f-a51f-20018193f978', 'https://is.gd/u8Vva0'),
+       ('fc834822-a1a7-44be-ba3a-d6678e0bd880', 'https://is.gd/u8Vva0'),
+       ('0fe3af4f-dc9c-4d8f-a39f-b9e992e83a84', 'https://is.gd/u8Vva0'),
+
+       ('8edaff03-27ca-4f88-9bd0-d26f94a91034', 'https://is.gd/xoyF5J'),
+       ('7212ecb7-f272-4859-8e4b-0828d589b4be', 'https://is.gd/xoyF5J'),
+       ('7212ecb7-f272-4859-8e4b-0828d589b4be', 'https://is.gd/xoyF5J'),
+       ('cc268ce4-333c-47f4-a877-665a72fcb0ec', 'https://is.gd/xoyF5J'),
+       ('6bcd0c90-1a5b-40a3-b1e0-f176524b9189', 'https://is.gd/xoyF5J'),
+       ('e888de78-7a57-4a72-838c-3fc073b7df93', 'https://is.gd/xoyF5J'),
+
+       ('775c3a06-234f-4e7d-b09e-002be69afba4', 'https://is.gd/VFUkj7'),
+       ('bd81f10f-0577-4fd0-843c-205082aee486', 'https://is.gd/VFUkj7'),
+       ('82255f61-f60c-4963-aadc-9b9cb9b8e453', 'https://is.gd/VFUkj7'),
+       ('7484c8ea-5002-41ab-a9e3-75a232ae52bc', 'https://is.gd/VFUkj7'),
+       ('e48fb02d-f002-458d-a976-0f9e2c5baaec', 'https://is.gd/VFUkj7'),
+       ('9255a778-b018-4788-94e0-bb8de8ef2c68', 'https://is.gd/VFUkj7'),
+
+       ('c4b404fe-f3a6-4996-b585-a76f8f2e8478', 'https://is.gd/ltXN3M'),
+       ('b07fdfd1-946e-4205-86a8-2cbaa9746ea6', 'https://is.gd/ltXN3M'),
+       ('1f07cf0f-898b-4a83-b46b-144f327a32e0', 'https://is.gd/ltXN3M'),
+       ('d74e6fa3-175a-4c61-a6d9-802cd4ce09fd', 'https://is.gd/ltXN3M'),
+       ('e349e217-3e91-4f7e-bd76-7f112b14973c', 'https://is.gd/ltXN3M'),
+
+       ('db908b6e-bc86-433f-ab4b-03e0caab8456', 'https://is.gd/qDrq5B'),
+       ('d4d1cc78-33d1-4e1a-b83a-5d6b717b8672', 'https://is.gd/qDrq5B'),
+       ('a66688d3-5c7b-4847-80d1-854eefd0a868', 'https://is.gd/qDrq5B'),
+       ('c39233b1-77ae-4ebe-8611-d3be92f4768c', 'https://is.gd/qDrq5B'),
+       ('ef97941b-e88d-4876-bc19-b59fac63fd17', 'https://is.gd/qDrq5B'),
+       ('b42d8cc8-ac10-4320-899e-2a23caf7d448', 'https://is.gd/qDrq5B'),
+
+       ('8f735ee5-dbbf-4c09-9363-e3938e09f3d1', 'https://is.gd/wDAxbR'),
+       ('99985e95-1346-4b16-a6ff-bad35919a6ee', 'https://is.gd/wDAxbR'),
+       ('c1e100fc-c065-46f0-9a40-56599092698a', 'https://is.gd/wDAxbR'),
+       ('9af131fd-2dd3-4b36-a888-14cd5c051963', 'https://is.gd/wDAxbR'),
+       ('554d9ff6-738d-4a99-a4e6-eb6bc7efaf27', 'https://is.gd/wDAxbR'),
+
+
+       ('3b19e783-8579-4639-8416-6ac6cb973661', 'https://is.gd/SfkVLl'),
+       ('d392d8c8-e8d8-49b7-b745-b4ac3360c08a', 'https://is.gd/SfkVLl'),
+       ('827de9bd-47d2-4da4-8a25-78001858f4f9', 'https://is.gd/SfkVLl'),
+       ('0674940e-e5de-41d5-9867-78f4496aa416', 'https://is.gd/SfkVLl'),
+       ('e7f05a21-3950-44f1-8cc0-4c2d91772ad9', 'https://is.gd/SfkVLl'),
+       ('d1df490d-3f47-431f-aa20-0c16a4830940', 'https://is.gd/SfkVLl'),
+
+
+       ('4012b7da-164a-4301-be2c-9f5219440a2d', 'https://is.gd/xAohgt'),
+       ('2c7fc1a3-db54-4c4b-8981-00c96cc10d33', 'https://is.gd/xAohgt'),
+       ('4d2fded8-cd85-43d6-9977-78a3eb6c4a3b', 'https://is.gd/xAohgt'),
+       ('a215abec-0274-46c5-aef5-bdb90f90ba53', 'https://is.gd/xAohgt'),
+       ('a194cbcc-77a9-4849-86e0-c33f0dc0d1bb', 'https://is.gd/xAohgt'),
+
+       ('d8d47640-da0d-4a6e-922f-91790fd9c041', 'https://is.gd/dKiro0'),
+       ('e37a4141-4856-46da-bfae-f4b7ce7d7ce7', 'https://is.gd/dKiro0'),
+       ('224e304c-079f-4e92-8cde-e6cce99af0fe', 'https://is.gd/dKiro0'),
+       ('79cfe883-ca13-414a-98a4-e55c8be576f9', 'https://is.gd/dKiro0'),
+       ('813f399f-fed7-4417-a263-02571ed6621f', 'https://is.gd/dKiro0'),
+
+       ('8f397dc4-847e-4d74-b002-fccefaeaaa08', 'https://is.gd/6b5m1r'),
+       ('fa48fc96-7f08-4a5a-b3b3-409740d3b115', 'https://is.gd/6b5m1r'),
+       ('613b07d6-21d8-4c35-8db9-c639e9237c87', 'https://is.gd/6b5m1r'),
+       ('bcb2810a-e01d-4921-9298-fc445e7c17d3', 'https://is.gd/6b5m1r'),
+       ('5f9491f4-7bda-4423-9c7d-56d45516d2f2', 'https://is.gd/6b5m1r'),
+       ('d94ca6d3-ef7c-4c27-b76b-1b601f7514bd', 'https://is.gd/6b5m1r');
+
