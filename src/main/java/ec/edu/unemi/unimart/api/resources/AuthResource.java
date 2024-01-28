10eb37560b2d691f35d381b56ab5c8149c19099b
Víctor Arreaga <arreagav17@gmail.com>
2024-01-27 22:48:06 -0500

    Implements resource
    Took 2 minutes

--- src/main/java/ec/edu/unemi/unimart/api/resources/AuthResource.java
+++ src/main/java/ec/edu/unemi/unimart/api/resources/AuthResource.java
@@ -1,46 +1,36 @@
 package ec.edu.unemi.unimart.api.resources;
 
-import ec.edu.unemi.unimart.api.dtos.login.LoginRequestDto;
-import ec.edu.unemi.unimart.api.dtos.login.LoginResponseDto;
-import ec.edu.unemi.unimart.services.jwt.JwtService;
-import ec.edu.unemi.unimart.services.jwt.JwtUserDetailsService;
+import ec.edu.unemi.unimart.api.dtos.auth.AuthResponseDto;
+import ec.edu.unemi.unimart.api.dtos.auth.LoginRequestDto;
+import ec.edu.unemi.unimart.api.dtos.auth.RegisterRequestDto;
+import ec.edu.unemi.unimart.services.auth.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
 public class AuthResource {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
-    private final JwtUserDetailsService jwtUserDetailsService;
+    private final AuthService authService;
 
-    @PutMapping("login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) throws Exception {
-        authenticate(loginRequestDto.username(), loginRequestDto.password());
-        UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(loginRequestDto.username());
-        return new ResponseEntity<>(jwtService.generateToken(userDetails), HttpStatus.OK);
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) throws Exception {
+        return new ResponseEntity<>(authService.authenticate(loginRequestDto), HttpStatus.OK);
     }
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
 
+    @PostMapping("register")
+    public ResponseEntity<AuthResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto) {
+        AuthResponseDto authResponseDto = authService.register(registerRequestDto);
+        return new ResponseEntity<>(authResponseDto, HttpStatus.CREATED);
     }
 }
