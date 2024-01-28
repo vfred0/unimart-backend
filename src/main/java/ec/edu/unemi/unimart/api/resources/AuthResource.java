80485dbc04154ee93798f387dee7de428bbc7086
Víctor Arreaga <arreagav17@gmail.com>
2024-01-27 22:48:25 -0500

    Implements resource
    Took 18 seconds

--- src/main/java/ec/edu/unemi/unimart/api/resources/AuthResource.java
+++ src/main/java/ec/edu/unemi/unimart/api/resources/AuthResource.java
@@ -5,15 +5,12 @@ import ec.edu.unemi.unimart.api.dtos.auth.LoginRequestDto;
 import ec.edu.unemi.unimart.api.dtos.auth.RegisterRequestDto;
 import ec.edu.unemi.unimart.services.auth.AuthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
