c52010cf333b20a0579bdeb8de2812ced52aa5e2
Víctor Arreaga <arreagav17@gmail.com>
2024-01-27 22:48:43 -0500

    Refactored
    Took 17 seconds

--- src/main/java/ec/edu/unemi/unimart/api/resources/UserResource.java
+++ src/main/java/ec/edu/unemi/unimart/api/resources/UserResource.java
@@ -1,19 +1,21 @@
 package ec.edu.unemi.unimart.api.resources;
 
 import ec.edu.unemi.unimart.api.dtos.ArticleDto;
+import ec.edu.unemi.unimart.api.dtos.UserDto;
 import ec.edu.unemi.unimart.services.user.IUserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ec.edu.unemi.unimart.api.dtos.UserDto;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("users")
 public class UserResource {
 
@@ -22,12 +24,6 @@ public class UserResource {
     @GetMapping("{id}/articles")
     ResponseEntity<List<ArticleDto>> getArticlesByUserId(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getArticlesByUserId(id));
-    }
-
-    @PostMapping
-    ResponseEntity<HttpHeaders> save(@RequestBody UserDto userDto) {
-        UUID articleId = userService.save(userDto);
-        return new ResponseEntity<>(HttpHeader.getHttpHeaders(articleId), HttpStatus.CREATED);
     }
 
     @PutMapping("{userId}")
