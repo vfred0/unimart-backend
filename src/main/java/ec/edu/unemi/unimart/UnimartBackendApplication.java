1a19b31390e8ec3c2b1d45bcce226fb847c5e27d
Víctor Arreaga <arreagav17@gmail.com>
2024-01-28 15:12:39 -0500

    For spring security debug
    Took 18 minutes

--- src/main/java/ec/edu/unemi/unimart/UnimartBackendApplication.java
+++ src/main/java/ec/edu/unemi/unimart/UnimartBackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableWebSecurity(debug = true)
 public class UnimartBackendApplication {
 
     public static void main(String[] args) {
