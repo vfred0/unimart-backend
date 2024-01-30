e87fb55c043ef361883a2893b999492d70e655b4
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 15:39:17 -0500

    Refactored, used tables `UserAccount` and `UserRole`
    Took 16 seconds

--- src/main/java/ec/edu/unemi/unimart/api/dtos/auth/RegisterRequestDto.java
+++ src/main/java/ec/edu/unemi/unimart/api/dtos/auth/RegisterRequestDto.java
@@ -1,12 +1,21 @@
 package ec.edu.unemi.unimart.api.dtos.auth;
 
 import ec.edu.unemi.unimart.data.enums.Role;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
 public record RegisterRequestDto(
+        @Size(max = 60)
         String names,
+        @Size(max = 50)
+        String photo,
         String username,
-        String password
+        String password,
+        @Size(max = 10)
+        String numberWhatsapp,
+        @Size(max = 250)
+        String about,
+        Set<Role> roles
 ) {
 }
