8a35cce43e96680d2578a59374b43144dcdb99d6
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 15:37:36 -0500

    Refactored with schema identity for tables
    Took 30 seconds

--- src/main/java/ec/edu/unemi/unimart/data/daos/IUserRepository.java
+++ src/main/java/ec/edu/unemi/unimart/data/daos/IUserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface IUserRepository extends IRepository<User, UUID> {
-    Optional<User> findByUsername(String username);
+
 }
--- src/main/java/ec/edu/unemi/unimart/data/daos/IUserRoleRepository.java
+++ src/main/java/ec/edu/unemi/unimart/data/daos/IUserRoleRepository.java
@@ -1,6 +1,6 @@
 package ec.edu.unemi.unimart.data.daos;
 
-import ec.edu.unemi.unimart.data.entities.UserRole;
+import ec.edu.unemi.unimart.data.entities.identity.UserRole;
 import ec.edu.unemi.unimart.data.enums.Role;
 
 import java.util.Optional;
@@ -9,4 +9,5 @@ import java.util.UUID;
 public interface IUserRoleRepository extends IRepository<UserRole, UUID> {
 
     Optional<UserRole> findByName(Role name);
+
 }
