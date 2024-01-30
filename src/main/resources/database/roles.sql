badb6101348fcadffe8ecd99c58426c302ea8183
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 00:23:38 -0500

    Query for roles
    Took 23 seconds

--- src/main/resources/database/roles.sql
+++ src/main/resources/database/roles.sql
@@ -1,3 +1,3 @@
-INSERT INTO roles(id, authority)
-VALUES (gen_random_uuid(), 'ADMIN'),
-       (gen_random_uuid(), 'AUTHENTICATED');
+INSERT INTO roles(id, name)
+VALUES (gen_random_uuid(), 'ROLE_ADMIN'),
+       (gen_random_uuid(), 'ROLE_AUTHENTICATED');
