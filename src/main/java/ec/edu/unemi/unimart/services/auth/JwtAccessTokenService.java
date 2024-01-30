56af01c21f93f1b1ade3465ae81b49c0c1e0890b
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 15:51:38 -0500

    Refactored
    Took 8 minutes

--- src/main/java/ec/edu/unemi/unimart/services/auth/JwtAccessTokenService.java
+++ src/main/java/ec/edu/unemi/unimart/services/auth/JwtAccessTokenService.java
@@ -1,8 +1,8 @@
-package ec.edu.unemi.unimart.services.jwt;
+package ec.edu.unemi.unimart.services.auth;
 
 import ec.edu.unemi.unimart.exceptions.MessageException;
 import ec.edu.unemi.unimart.exceptions.NotFoundException;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,42 +17,42 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Slf4j
+@RequiredArgsConstructor
 public class JwtAccessTokenService {
 
     private final JwtEncoder jwtEncoder;
 
-    public JwtAccessTokenService(JwtEncoder jwtEncoder) {
-        this.jwtEncoder = jwtEncoder;
-    }
-
-    public String generateIdToken(Authentication authentication) {
-        UserDetails userDetails = Optional
+    private static UserDetails getUserDetails(Authentication authentication) {
+        return Optional
                 .of(authentication.getPrincipal())
                 .filter(UserDetails.class::isInstance)
                 .map(UserDetails.class::cast)
                 .orElseThrow(() -> NotFoundException.throwBecauseOf(MessageException.USER_NOT_FOUND));
+    }
 
-        List<String> roles = userDetails
+    private static JwtClaimsSet getClaims(UserDetails userDetails) {
+        int AMOUNT_TO_ADD = 1;
+        String SCOPE = "scope";
+        return JwtClaimsSet
+                .builder()
+                .claim(SCOPE, getRoles(userDetails))
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plus(AMOUNT_TO_ADD, ChronoUnit.DAYS))
+                .subject(userDetails.getUsername())
+                .build();
+    }
+
+    private static List<String> getRoles(UserDetails userDetails) {
+        return userDetails
                 .getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .toList();
+    }
 
-        log.info("Roles: {}", roles);
-
-        Instant issuedAt = Instant.now();
-        Instant expiresAt = issuedAt.plus(1, ChronoUnit.DAYS);
-
-        JwtClaimsSet claimsSet = JwtClaimsSet
-                .builder()
-                .claim("scope", roles)
-                .issuedAt(issuedAt)
-                .expiresAt(expiresAt)
-                .subject(userDetails.getUsername())
-                .build();
-
-
+    public String generateIdToken(Authentication authentication) {
+        UserDetails userDetails = getUserDetails(authentication);
+        JwtClaimsSet claimsSet = getClaims(userDetails);
         return this.jwtEncoder
                 .encode(JwtEncoderParameters.from(claimsSet))
                 .getTokenValue();
