dff1322a1e9fbe16f97cec33ce67223ec6ebf9c0
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 16:11:54 -0500

    Implements RSA Keys from public and private key
    Took 19 minutes

--- src/main/java/ec/edu/unemi/unimart/UnimartBackendApplication.java
+++ src/main/java/ec/edu/unemi/unimart/UnimartBackendApplication.java
@@ -1,9 +1,12 @@
 package ec.edu.unemi.unimart;
 
+import ec.edu.unemi.unimart.configurations.RsaKeys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeys.class)
 public class UnimartBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/ec/edu/unemi/unimart/configurations/RsaKeys.java
+++ src/main/java/ec/edu/unemi/unimart/configurations/RsaKeys.java
@@ -1,7 +1,10 @@
-package ec.edu.unemi.unimart.configurations.rsa;
+package ec.edu.unemi.unimart.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+@ConfigurationProperties(prefix = "rsa-keys")
 public record RsaKeys(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
 }
