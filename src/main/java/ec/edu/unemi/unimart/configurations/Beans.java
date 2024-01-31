5edffe0166b51498d35586a802e1a31eeffccf8b
Víctor Arreaga <arreagav17@gmail.com>
2024-01-30 23:27:05 -0500

    Refactored
    Took 29 seconds

--- src/main/java/ec/edu/unemi/unimart/configurations/Beans.java
+++ src/main/java/ec/edu/unemi/unimart/configurations/Beans.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
