231e129e5afe298b15c250bb02b015b55c23bf12
Víctor Arreaga <arreagav17@gmail.com>
2024-01-31 22:10:04 -0500

    Added beans audit aware

--- src/main/java/ec/edu/unemi/unimart/data/entities/auditing/AuditorAwareImpl.java
+++ src/main/java/ec/edu/unemi/unimart/data/entities/auditing/AuditorAwareImpl.java
@@ -1,11 +1,12 @@
 package ec.edu.unemi.unimart.data.entities.auditing;
 
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
-public class AuditorAware implements org.springframework.data.domain.AuditorAware<String> {
+public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
