000ea5442857f34654fef5f9dcd58b1ae470353c
Víctor Arreaga <arreagav17@gmail.com>
2024-01-31 22:13:51 -0500

    Refactored enums

--- src/main/java/ec/edu/unemi/unimart/data/enums/ArticleType.java
+++ src/main/java/ec/edu/unemi/unimart/data/enums/ArticleType.java
@@ -1,21 +1,7 @@
 package ec.edu.unemi.unimart.data.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.Arrays;
-
-@Getter
-@AllArgsConstructor
 public enum ArticleType {
-    PUBLISHED("Publicado"),
-    PROPOSED("Propuesto"),
-    EXCHANGED("Intercambiado");
-
-    private final String name;
-    @Override
-    public String toString() {
-        return name;
-    }
-
+    PUBLISHED,
+    PROPOSED,
+    EXCHANGED
 }
